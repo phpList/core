1b83504731bf962e96794aa84af5ddf359e58a5d
Sam Tuke <mail@samtuke.com>
2015-02-12 20:41:57 +0100

    Started work on MailingListTest tests; Added test for getAllLists()

--- core/tests/MailingListTest.php
+++ core/tests/MailingListTest.php
@@ -1,11 +1,25 @@
 <?php
-namespace phpList\test;
+
+// FIXME: Autoloading failing for helper classes. These requires are a workaround
+require_once( dirname( __FILE__ ) . '/../helper/Logger.php' );
+require_once( dirname( __FILE__ ) . '/../helper/Database.php' );
 
 use phpList\Admin;
 use phpList\Config;
+use phpList\helper;
 use phpList\MailingList;
 
 class MailingListTest extends \PHPUnit_Framework_TestCase {
+
+    public function setUp()
+    {
+        $this->configFile = dirname( __FILE__ ) . '/../../config.ini';
+        $this->logger = new phplist\helper\Logger();
+        $this->config = new Config( $this->configFile );
+        $this->database = new phplist\helper\Database( $this->config, $this->logger );
+        $this->mailingList = new MailingList( $this->config, $this->database );
+    }
+
     public function testCreateList()
     {
         //first need to create an admin
@@ -28,5 +42,9 @@ class MailingListTest extends \PHPUnit_Framework_TestCase {
         $this->assertTrue(($refetchedList->name === $updated_list_name));
 
     }
+
+    public function testGetAllLists()
+    {
+        $result = $this->mailingList->getAllLists();
+    }
 }
- 
