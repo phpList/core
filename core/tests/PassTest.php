0c3ce53335833940d9fa02e818bebce159ae8f50
Sam Tuke <mail@samtuke.com>
2016-03-16 19:58:50 +0100

    Adapted symfony containers for class loading

--- core/tests/PassTest.php
+++ core/tests/PassTest.php
@@ -3,11 +3,25 @@ namespace phpList\test;
 
 use phpList\Pass;
 
+// Symfony namespaces
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
 class PassTest extends \PHPUnit_Framework_TestCase {
 
     public function setUp()
     {
-        $this->pass = new Pass();
+        // Create Symfony DI service container object for use by other classes
+        $this->container = new ContainerBuilder();
+        // Create new Symfony file loader to handle the YAML service config file
+        $loader = new YamlFileLoader( $this->container, new FileLocator(__DIR__) );
+        // Load the service config file, which is in YAML format
+        $loader->load( '../services.yml' );
+        // Get objects from container
+        $this->pass = $this->container->get( 'Pass' );
+        // Set default pwd for testing
         $this->plainPass = 'easypassword';
     }
 
