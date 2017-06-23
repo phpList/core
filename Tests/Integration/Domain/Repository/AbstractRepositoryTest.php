02b2438b0b1c64e7849338222cdbe121c7ef038a
Oliver Klee <github@oliverklee.de>
2017-06-23 21:01:23 +0200

    [FEATURE] Add an abstract base class for repository integration tests (#88)

---
+++ Tests/Integration/Domain/Repository/AbstractRepositoryTest.php
@@ -0,0 +1,137 @@
+<?php
+declare(strict_types=1);
+
+namespace PhpList\PhpList4\Tests\Integration\Domain\Repository;
+
+use PhpList\PhpList4\Core\Bootstrap;
+use PhpList\PhpList4\Domain\Model\Interfaces\Identity;
+use PHPUnit\DbUnit\Database\Connection;
+use PHPUnit\DbUnit\DataSet\CsvDataSet;
+use PHPUnit\DbUnit\TestCaseTrait;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * This is the base class for all repository integration tests.
+ *
+ * Make sure to call parent::setUp() first thing in your setUp method.
+ *
+ * @author Oliver Klee <oliver@phplist.com>
+ */
+abstract class AbstractRepositoryTest extends TestCase
+{
+    use TestCaseTrait;
+
+    /**
+     * @var Connection
+     */
+    private $databaseConnection = null;
+
+    /**
+     * @var \PDO
+     */
+    private static $pdo = null;
+
+    /**
+     * @var CsvDataSet
+     */
+    private $dataSet = null;
+
+    /**
+     * @var Bootstrap
+     */
+    protected $bootstrap = null;
+
+    protected function setUp()
+    {
+        $this->bootstrap = Bootstrap::getInstance()->configure();
+        $this->initializeDatabaseTester();
+    }
+
+    /**
+     * Initializes the CSV data set and the database tester.
+     *
+     * @return void
+     */
+    protected function initializeDatabaseTester()
+    {
+        $this->dataSet = new CsvDataSet();
+
+        $this->databaseTester = null;
+        $this->getDatabaseTester()->setSetUpOperation($this->getSetUpOperation());
+    }
+
+    protected function tearDown()
+    {
+        $this->getDatabaseTester()->setTearDownOperation($this->getTearDownOperation());
+        $this->getDatabaseTester()->setDataSet($this->getDataSet());
+        $this->getDatabaseTester()->onTearDown();
+
+        // Destroy the tester after the test is run to keep DB connections
+        // from piling up.
+        $this->databaseTester = null;
+
+        Bootstrap::purgeInstance();
+    }
+
+    /**
+     * Returns the test database connection.
+     *
+     * @return Connection
+     */
+    protected function getConnection(): Connection
+    {
+        if ($this->databaseConnection === null) {
+            if (self::$pdo === null) {
+                self::$pdo = new \PDO(
+                    'mysql:dbname=' . getenv('PHPLIST_DATABASE_NAME'),
+                    getenv('PHPLIST_DATABASE_USER'),
+                    getenv('PHPLIST_DATABASE_PASSWORD')
+                );
+            }
+            $this->databaseConnection = $this->createDefaultDBConnection(self::$pdo);
+        }
+
+        return $this->databaseConnection;
+    }
+
+    /**
+     * Returns the test data set.
+     *
+     * Add data to in the individual test by calling $this->getDataSet()->addTable.
+     *
+     * @return CsvDataSet
+     */
+    protected function getDataSet(): CsvDataSet
+    {
+        return $this->dataSet;
+    }
+
+    /**
+     * Applies all database changes on $this->dataSet.
+     *
+     * This methods needs to be called after the last addTable call in each test.
+     *
+     * @return void
+     */
+    protected function applyDatabaseChanges()
+    {
+        $this->getDatabaseTester()->setDataSet($this->getDataSet());
+        $this->getDatabaseTester()->onSetUp();
+    }
+
+    /**
+     * Sets the (private) ID of $subject.
+     *
+     * @param Identity $subject
+     * @param int $id
+     *
+     * @return void
+     */
+    protected function setId(Identity $subject, int $id)
+    {
+        $reflectionObject = new \ReflectionObject($subject);
+        $reflectionProperty = $reflectionObject->getProperty('id');
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($subject, $id);
+    }
+}
--- Tests/Integration/Domain/Repository/Identity/AdministratorTokenRepositoryTest.php
+++ Tests/Integration/Domain/Repository/Identity/AdministratorTokenRepositoryTest.php
@@ -4,42 +4,21 @@ declare(strict_types=1);
 namespace PhpList\PhpList4\Tests\Integration\Domain\Repository\Identity;
 
 use Doctrine\Common\Persistence\ObjectRepository;
-use PhpList\PhpList4\Core\Bootstrap;
 use PhpList\PhpList4\Domain\Model\Identity\AdministratorToken;
 use PhpList\PhpList4\Domain\Repository\Identity\AdministratorTokenRepository;
-use PHPUnit\DbUnit\Database\Connection;
-use PHPUnit\DbUnit\DataSet\CsvDataSet;
-use PHPUnit\DbUnit\TestCaseTrait;
-use PHPUnit\Framework\TestCase;
+use PhpList\PhpList4\Tests\Integration\Domain\Repository\AbstractRepositoryTest;
 
 /**
  * Testcase.
  *
  * @author Oliver Klee <oliver@phplist.com>
  */
-class AdministratorTokenRepositoryTest extends TestCase
+class AdministratorTokenRepositoryTest extends AbstractRepositoryTest
 {
-    use TestCaseTrait;
-
     /**
      * @var string
      */
     const TABLE_NAME = 'phplist_admintoken';
-
-    /**
-     * @var Connection
-     */
-    private $databaseConnection = null;
-
-    /**
-     * @var \PDO
-     */
-    private static $pdo = null;
-
-    /**
-     * @var CsvDataSet
-     */
-    private $dataSet = null;
 
     /**
      * @var AdministratorTokenRepository|ObjectRepository
@@ -48,88 +27,9 @@ class AdministratorTokenRepositoryTest extends TestCase
 
     protected function setUp()
     {
-        $bootstrap = Bootstrap::getInstance()->configure();
-        $this->subject = $bootstrap->getEntityManager()->getRepository(AdministratorToken::class);
+        parent::setUp();
 
-        $this->dataSet = new CsvDataSet();
-
-        $this->databaseTester = null;
-        $this->getDatabaseTester()->setSetUpOperation($this->getSetUpOperation());
-    }
-
-    protected function tearDown()
-    {
-        $this->getDatabaseTester()->setTearDownOperation($this->getTearDownOperation());
-        $this->getDatabaseTester()->setDataSet($this->getDataSet());
-        $this->getDatabaseTester()->onTearDown();
-
-        // Destroy the tester after the test is run to keep DB connections
-        // from piling up.
-        $this->databaseTester = null;
-
-        Bootstrap::purgeInstance();
-    }
-
-    /**
-     * Returns the test database connection.
-     *
-     * @return Connection
-     */
-    protected function getConnection(): Connection
-    {
-        if ($this->databaseConnection === null) {
-            if (self::$pdo === null) {
-                self::$pdo = new \PDO(
-                    'mysql:dbname=' . getenv('PHPLIST_DATABASE_NAME'),
-                    getenv('PHPLIST_DATABASE_USER'),
-                    getenv('PHPLIST_DATABASE_PASSWORD')
-                );
-            }
-            $this->databaseConnection = $this->createDefaultDBConnection(self::$pdo);
-        }
-
-        return $this->databaseConnection;
-    }
-
-    /**
-     * Returns the test data set.
-     *
-     * Add data to in the individual test by calling $this->getDataSet()->addTable.
-     *
-     * @return CsvDataSet
-     */
-    protected function getDataSet(): CsvDataSet
-    {
-        return $this->dataSet;
-    }
-
-    /**
-     * Applies all database changes on $this->dataSet.
-     *
-     * This methods needs to be called after the last addTable call in each test.
-     *
-     * @return void
-     */
-    private function applyDatabaseChanges()
-    {
-        $this->getDatabaseTester()->setDataSet($this->getDataSet());
-        $this->getDatabaseTester()->onSetUp();
-    }
-
-    /**
-     * Sets the (private) ID of $subject.
-     *
-     * @param AdministratorToken $subject
-     * @param int $id
-     *
-     * @return void
-     */
-    private function setId(AdministratorToken $subject, int $id)
-    {
-        $reflectionObject = new \ReflectionObject($subject);
-        $reflectionProperty = $reflectionObject->getProperty('id');
-        $reflectionProperty->setAccessible(true);
-        $reflectionProperty->setValue($subject, $id);
+        $this->subject = $this->bootstrap->getEntityManager()->getRepository(AdministratorToken::class);
     }
 
     /**
