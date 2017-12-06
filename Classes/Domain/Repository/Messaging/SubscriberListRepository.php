1dc6aaf02700a7bd9d16457293137cfc6385bec7
Oliver Klee <github@oliverklee.de>
2017-12-06 20:04:25 +0100

    [FEATURE] SubscriberListRepository::findByOwner (#225)

--- Classes/Domain/Repository/Messaging/SubscriberListRepository.php
+++ Classes/Domain/Repository/Messaging/SubscriberListRepository.php
@@ -3,10 +3,14 @@ declare(strict_types=1);
 
 namespace PhpList\PhpList4\Domain\Repository\Messaging;
 
+use PhpList\PhpList4\Domain\Model\Identity\Administrator;
+use PhpList\PhpList4\Domain\Model\Messaging\SubscriberList;
 use PhpList\PhpList4\Domain\Repository\AbstractRepository;
 
 /**
  * Repository for SubscriberList models.
+ *
+ * @method SubscriberList[] findByOwner(Administrator $owner)
  *
  * @author Oliver Klee <oliver@phplist.com>
  */
