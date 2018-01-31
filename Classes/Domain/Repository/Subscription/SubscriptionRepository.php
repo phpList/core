cb0176de4bfab2a1615d31d1d2e10bb6b5d8354e
Oliver Klee <oliver@phplist.com>
2018-01-31 20:20:45 +0100

    [FEATURE] Repository methods for querying subscriptions
    - SubscriptionRepository.findBySubscriber
    - SubscriptionRepository.findBySubscriberList

--- Classes/Domain/Repository/Subscription/SubscriptionRepository.php
+++ Classes/Domain/Repository/Subscription/SubscriptionRepository.php
@@ -3,14 +3,16 @@ declare(strict_types=1);
 
 namespace PhpList\PhpList4\Domain\Repository\Subscription;
 
+use PhpList\PhpList4\Domain\Model\Messaging\SubscriberList;
+use PhpList\PhpList4\Domain\Model\Subscription\Subscriber;
 use PhpList\PhpList4\Domain\Model\Subscription\Subscription;
 use PhpList\PhpList4\Domain\Repository\AbstractRepository;
 
 /**
  * Repository for Subscription models.
  *
- * @method Subscription[] findBySubscriber(int $subscriberId)
- * @method Subscription[] findBySubscriberList(int $listId)
+ * @method Subscription[] findBySubscriber(Subscriber $subscriber)
+ * @method Subscription[] findBySubscriberList(SubscriberList $list)
  *
  * @author Oliver Klee <oliver@phplist.com>
  */
