a396ce8bf3e898b730ba737a43e900eb07e39c49
Sam Tuke <mail@samtuke.com>
2019-04-19 20:58:01 +0200

    Add initial Template repository

---
+++ src/Domain/Repository/Messaging/TemplateRepository.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace PhpList\Core\Domain\Repository\Messaging;
+
+use PhpList\Core\Domain\Model\Identity\Administrator;
+use PhpList\Core\Domain\Model\Messaging\Template;
+use PhpList\Core\Domain\Repository\AbstractRepository;
+
+/**
+ * Repository for Template models.
+ *
+ * @author Sam Tuke <sam@phplist.com>
+ */
+class TemplateRepository extends AbstractRepository
+{
+}
