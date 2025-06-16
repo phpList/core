3418bd4dc36d37057482cd3ce9c1dd2a5947c8e1
Tatevik <tatevikg1@gmail.com>
2025-06-16 10:57:29 +0400

    ISSUE-345: remove not used imports

--- src/Domain/Identity/Service/AdministratorManager.php
+++ src/Domain/Identity/Service/AdministratorManager.php
@@ -5,12 +5,9 @@ declare(strict_types=1);
 namespace PhpList\Core\Domain\Identity\Service;
 
 use Doctrine\ORM\EntityManagerInterface;
-use InvalidArgumentException;
 use PhpList\Core\Domain\Identity\Model\Administrator;
 use PhpList\Core\Domain\Identity\Model\Dto\CreateAdministratorDto;
 use PhpList\Core\Domain\Identity\Model\Dto\UpdateAdministratorDto;
-use PhpList\Core\Domain\Identity\Model\PrivilegeFlag;
-use PhpList\Core\Domain\Identity\Model\Privileges;
 use PhpList\Core\Security\HashGenerator;
 
 class AdministratorManager
