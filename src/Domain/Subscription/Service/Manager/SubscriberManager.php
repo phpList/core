9a9c937e83c4dbd090bbaa9db48ce2f7e9f639a6
Tatevik <tatevikg1@gmail.com>
2025-06-19 16:36:24 +0400

    ISSUE-12: subscription confirmation

--- src/Domain/Subscription/Service/Manager/SubscriberManager.php
+++ src/Domain/Subscription/Service/Manager/SubscriberManager.php
@@ -5,22 +5,32 @@ declare(strict_types=1);
 namespace PhpList\Core\Domain\Subscription\Service\Manager;
 
 use Doctrine\ORM\EntityManagerInterface;
+use PhpList\Core\Domain\Messaging\Service\EmailService;
 use PhpList\Core\Domain\Subscription\Model\Dto\CreateSubscriberDto;
 use PhpList\Core\Domain\Subscription\Model\Dto\ImportSubscriberDto;
 use PhpList\Core\Domain\Subscription\Model\Dto\UpdateSubscriberDto;
 use PhpList\Core\Domain\Subscription\Model\Subscriber;
 use PhpList\Core\Domain\Subscription\Repository\SubscriberRepository;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Mime\Email;
 
 class SubscriberManager
 {
     private SubscriberRepository $subscriberRepository;
     private EntityManagerInterface $entityManager;
+    private EmailService $emailService;
+    private string $confirmationUrl;
 
-    public function __construct(SubscriberRepository $subscriberRepository, EntityManagerInterface $entityManager)
-    {
+    public function __construct(
+        SubscriberRepository $subscriberRepository, 
+        EntityManagerInterface $entityManager,
+        EmailService $emailService,
+        string $confirmationUrl
+    ) {
         $this->subscriberRepository = $subscriberRepository;
         $this->entityManager = $entityManager;
+        $this->emailService = $emailService;
+        $this->confirmationUrl = $confirmationUrl;
     }
 
     public function createSubscriber(CreateSubscriberDto $subscriberDto): Subscriber
@@ -35,7 +45,52 @@ class SubscriberManager
 
         $this->subscriberRepository->save($subscriber);
 
+        if ($subscriberDto->requestConfirmation) {
+            $this->sendConfirmationEmail($subscriber);
+        }
+
         return $subscriber;
+    }
+
+    /**
+     * Send a confirmation email to the subscriber
+     */
+    private function sendConfirmationEmail(Subscriber $subscriber): void
+    {
+        $confirmationLink = $this->generateConfirmationLink($subscriber);
+
+        $subject = 'Please confirm your subscription';
+        $textContent = "Thank you for subscribing!\n\n"
+            . "Please confirm your subscription by clicking the link below:\n"
+            . $confirmationLink . "\n\n"
+            . "If you did not request this subscription, please ignore this email.";
+
+        $htmlContent = '';
+        if ($subscriber->hasHtmlEmail()) {
+            $htmlContent = "<p>Thank you for subscribing!</p>"
+                . "<p>Please confirm your subscription by clicking the link below:</p>"
+                . "<p><a href=\"" . $confirmationLink . "\">Confirm Subscription</a></p>"
+                . "<p>If you did not request this subscription, please ignore this email.</p>";
+        }
+
+        $email = (new Email())
+            ->to($subscriber->getEmail())
+            ->subject($subject)
+            ->text($textContent);
+
+        if (!empty($htmlContent)) {
+            $email->html($htmlContent);
+        }
+
+        $this->emailService->sendEmail($email);
+    }
+
+    /**
+     * Generate a confirmation link for the subscriber
+     */
+    private function generateConfirmationLink(Subscriber $subscriber): string
+    {
+        return $this->confirmationUrl . $subscriber->getUniqueId();
     }
 
     public function getSubscriber(int $subscriberId): Subscriber
