9ba5c10e97ffa5a3c655cc81a97407958291154d
TatevikGr <tatevik@phplist.com>
2025-07-24 12:21:34 +0400

    Subscriber history service (#348)
    Co-authored-by: Tatevik <tatevikg1@gmail.com>

---
+++ src/Domain/Subscription/Model/Filter/SubscriptionHistoryFilter.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpList\Core\Domain\Subscription\Model\Filter;
+
+use DateTimeImmutable;
+use PhpList\Core\Domain\Common\Model\Filter\FilterRequestInterface;
+use PhpList\Core\Domain\Subscription\Model\Subscriber;
+
+class SubscriptionHistoryFilter implements FilterRequestInterface
+{
+    private ?Subscriber $subscriber;
+    private ?string $ip;
+    private ?DateTimeImmutable $dateFrom;
+    private ?string $summery;
+
+    public function __construct(
+        ?Subscriber $subscriber = null,
+        ?string $ip = null,
+        ?DateTimeImmutable $dateFrom = null,
+        ?string $summery = null,
+    ) {
+        $this->subscriber = $subscriber;
+        $this->ip = $ip;
+        $this->dateFrom = $dateFrom;
+        $this->summery = $summery;
+    }
+
+    public function getSubscriber(): ?Subscriber
+    {
+        return $this->subscriber;
+    }
+
+    public function getIp(): ?string
+    {
+        return $this->ip;
+    }
+
+    public function getDateFrom(): ?DateTimeImmutable
+    {
+        return $this->dateFrom;
+    }
+
+    public function getSummery(): ?string
+    {
+        return $this->summery;
+    }
+}
--- src/Domain/Subscription/Repository/SubscriberHistoryRepository.php
+++ src/Domain/Subscription/Repository/SubscriberHistoryRepository.php
@@ -4,11 +4,50 @@ declare(strict_types=1);
 
 namespace PhpList\Core\Domain\Subscription\Repository;
 
+use InvalidArgumentException;
+use PhpList\Core\Domain\Common\Model\Filter\FilterRequestInterface;
 use PhpList\Core\Domain\Common\Repository\AbstractRepository;
 use PhpList\Core\Domain\Common\Repository\CursorPaginationTrait;
 use PhpList\Core\Domain\Common\Repository\Interfaces\PaginatableRepositoryInterface;
+use PhpList\Core\Domain\Subscription\Model\Filter\SubscriptionHistoryFilter;
+use PhpList\Core\Domain\Subscription\Model\SubscriberHistory;
 
 class SubscriberHistoryRepository extends AbstractRepository implements PaginatableRepositoryInterface
 {
     use CursorPaginationTrait;
+
+    /**
+     * @return SubscriberHistory[]
+     * @throws InvalidArgumentException
+     */
+    public function getFilteredAfterId(int $lastId, int $limit, ?FilterRequestInterface $filter = null): array
+    {
+        $queryBuilder = $this->createQueryBuilder('sh');
+
+        if (!$filter instanceof SubscriptionHistoryFilter) {
+            throw new InvalidArgumentException('Expected SubscriptionHistoryFilter.');
+        }
+
+        if ($filter->getSubscriber() !== null) {
+            $queryBuilder->andWhere('sh.subscriber = :subscriber')
+                ->setParameter('subscriber', $filter->getSubscriber());
+        }
+
+        if ($filter->getDateFrom() !== null) {
+            $queryBuilder->andWhere('sh.date >= :date')
+                ->setParameter('date', $filter->getDateFrom());
+        }
+
+        if ($filter->getIp() !== null) {
+            $queryBuilder->andWhere('sh.ip = :ip')
+                ->setParameter('ip', $filter->getIp());
+        }
+
+        if ($filter->getSummery() !== null) {
+            $queryBuilder->andWhere('sh.summery = :summery')
+                ->setParameter('summery', $filter->getSummery());
+        }
+
+        return $queryBuilder->getQuery()->getResult();
+    }
 }
