113d6c2084801e44cd66f8344de5897c322c6a6f
Tatevik <tatevikg1@gmail.com>
2025-10-06 11:56:17 +0400

    replace list names

--- src/Domain/Messaging/MessageHandler/SubscriptionConfirmationMessageHandler.php
+++ src/Domain/Messaging/MessageHandler/SubscriptionConfirmationMessageHandler.php
@@ -7,8 +7,9 @@ namespace PhpList\Core\Domain\Messaging\MessageHandler;
 use PhpList\Core\Domain\Configuration\Model\ConfigOption;
 use PhpList\Core\Domain\Configuration\Service\Provider\ConfigProvider;
 use PhpList\Core\Domain\Configuration\Service\UserPersonalizer;
-use PhpList\Core\Domain\Messaging\Message\SubscriberConfirmationMessage;
+use PhpList\Core\Domain\Messaging\Message\SubscriptionConfirmationMessage;
 use PhpList\Core\Domain\Messaging\Service\EmailService;
+use PhpList\Core\Domain\Subscription\Repository\SubscriberListRepository;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Mime\Email;
@@ -23,27 +24,32 @@ class SubscriptionConfirmationMessageHandler
     private ConfigProvider $configProvider;
     private LoggerInterface $logger;
     private UserPersonalizer $userPersonalizer;
+    private SubscriberListRepository $subscriberListRepository;
 
     public function __construct(
         EmailService $emailService,
         ConfigProvider $configProvider,
         LoggerInterface $logger,
         UserPersonalizer $userPersonalizer,
+        SubscriberListRepository $subscriberListRepository,
     ) {
         $this->emailService = $emailService;
         $this->configProvider = $configProvider;
         $this->logger = $logger;
         $this->userPersonalizer = $userPersonalizer;
+        $this->subscriberListRepository = $subscriberListRepository;
     }
 
     /**
      * Process a subscription confirmation message by sending the confirmation email
      */
-    public function __invoke(SubscriberConfirmationMessage $message): void
+    public function __invoke(SubscriptionConfirmationMessage $message): void
     {
         $subject = $this->configProvider->getValue(ConfigOption::SubscribeEmailSubject);
         $textContent = $this->configProvider->getValue(ConfigOption::SubscribeMessage);
-        $replacedTextContent = $this->userPersonalizer->personalize($textContent, $message->getUniqueId());
+        $personalizedTextContent = $this->userPersonalizer->personalize($textContent, $message->getUniqueId());
+        $listOfLists = $this->getListNames($message->getListIds());
+        $replacedTextContent = str_replace('[LISTS]', $personalizedTextContent, $listOfLists);
 
         $email = (new Email())
             ->to($message->getEmail())
@@ -53,5 +59,18 @@ class SubscriptionConfirmationMessageHandler
         $this->emailService->sendEmail($email);
 
         $this->logger->info('Subscription confirmation email sent to {email}', ['email' => $message->getEmail()]);
+    }
+
+    private function getListNames(array $listIds): string
+    {
+        $listNames = [];
+        foreach ($listIds as $id) {
+            $list = $this->subscriberListRepository->find($id);
+            if ($list) {
+                $listNames[] = $list->getName();
+            }
+        }
+
+        return implode(', ', $listNames);
     }
 }
