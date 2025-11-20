0a635376503c6cb8223c89ffff52d23cbcee55ac
Tatevik <tatevikg1@gmail.com>
2025-11-20 11:43:00 +0400

    Update: Admin update request

--- src/Domain/Identity/Model/Dto/UpdateAdministratorDto.php
+++ src/Domain/Identity/Model/Dto/UpdateAdministratorDto.php
@@ -7,7 +7,6 @@ namespace PhpList\Core\Domain\Identity\Model\Dto;
 final class UpdateAdministratorDto
 {
     public function __construct(
-        public readonly int $administratorId,
         public readonly ?string $loginName = null,
         public readonly ?string $password = null,
         public readonly ?string $email = null,
